3aced373a24949e52738d51c5fd0921d9614aced
raghav2005 <raghavawasthi2005@gmail.com>
2022-08-03 17:18:23 +0400

    set up basic mysql database where /students automatically adds a pre-defined row to the table students in the database. TODO: create a form so that /students becomes a directory to dynamically add to the database - will help w/ my understanding for logins/sign ups

--- assembly_code_visualiser/app.js
+++ assembly_code_visualiser/app.js
@@ -5,9 +5,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var flash = require('express-flash');
+var session = require('express-session');
+var mysql = require('mysql');
+var connection = require('./lib/db');
+
 // all routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var studentRouter = require('./routes/student');
 
 // initialise express
 var app = express();
@@ -23,24 +29,36 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// session
+app.use(session({
+	cookie: { maxAge: 60000 },
+	store: new session.MemoryStore,
+	saveUninitialized: true,
+	resave: 'true',
+	secret: 'secret'
+}))
+
+app.use(flash());
+
 // initialise all routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/students', studentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+	next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+res.render('error');
 });
 
 module.exports = app;
---
+++ assembly_code_visualiser/lib/db.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+
+require('dotenv').config()
+
+var DB_HOST = process.env.DB_HOST
+var DB_USER = process.env.DB_USER
+var DB_PASSWORD = process.env.DB_PASSWORD
+var DB_DATABASE = process.env.DB_DATABASE
+var DB_PORT = process.env.DB_PORT
+
+var connection = mysql.createConnection({
+	host: DB_HOST,
+	user: DB_USER,
+	password: DB_PASSWORD,
+	database: DB_DATABASE,
+	port: DB_PORT
+})
+
+connection.connect(function (error) {
+	if (!!error) {
+		console.log(error);
+	} else {
+		console.log('Connected..!');
+	}
+});
+
+module.exports = connection;

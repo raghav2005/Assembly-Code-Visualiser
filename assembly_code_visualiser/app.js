c8f4ac0e37fdab245e524835aade41fbc3a68f95
raghav2005 <raghavawasthi2005@gmail.com>
2022-08-03 15:46:19 +0400

    code cleaned up + commented

--- assembly_code_visualiser/app.js
+++ assembly_code_visualiser/app.js
@@ -1,24 +1,29 @@
+// all packages
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// all routes
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+// initialise express
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// initialise all packages
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// initialise all routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 

baf2a13e7f601bde29baf5f7e02c7cbd8f45481a
raghav2005 <raghavawasthi2005@gmail.com>
2022-08-05 23:28:38 +0400

    basic layout and forms created for login & sign up pages

--- assembly_code_visualiser/routes/login_sign_up/sign_up.js
+++ assembly_code_visualiser/routes/login_sign_up/sign_up.js
@@ -6,4 +6,8 @@ router.get('/', function (req, res, next) {
 	res.render('login_sign_up/sign_up', { title: 'Sign Up', menu_id: 'login_sign_up' });
 });
 
+router.post('/sign_up', function (req, res, next) {
+	
+})
+
 module.exports = router;

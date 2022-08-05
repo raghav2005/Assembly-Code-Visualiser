6115579046687731ede41e41a0dff2e7429db154
raghav2005 <raghavawasthi2005@gmail.com>
2022-08-06 02:25:39 +0400

    added a tooltip for the navbar user indicator

--- assembly_code_visualiser/public/javascripts/menu.js
+++ assembly_code_visualiser/public/javascripts/menu.js
@@ -1,4 +1,10 @@
+// set active tab in the navbar automatically
 $(document).ready(function () {
 	var element = $('meta[name="active-menu"]').attr('content');
 	$('#' + element).addClass('active');
+});
+
+// activate tooltips (the thing that shows up underneath the navbar user indicator)
+$(document).ready(function () {
+	$('[data-toggle="tooltip"]').tooltip();
 });

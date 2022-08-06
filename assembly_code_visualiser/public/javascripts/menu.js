5f5e75e31002dde45e2538c43b418f9cc0ac8372
raghav2005 <raghavawasthi2005@gmail.com>
2022-08-06 23:17:12 +0400

    Revert "added a tooltip for the navbar user indicator"
    This reverts commit 6115579046687731ede41e41a0dff2e7429db154.

--- assembly_code_visualiser/public/javascripts/menu.js
+++ assembly_code_visualiser/public/javascripts/menu.js
@@ -1,10 +1,4 @@
-// set active tab in the navbar automatically
 $(document).ready(function () {
 	var element = $('meta[name="active-menu"]').attr('content');
 	$('#' + element).addClass('active');
-});
-
-// activate tooltips (the thing that shows up underneath the navbar user indicator)
-$(document).ready(function () {
-	$('[data-toggle="tooltip"]').tooltip();
 });

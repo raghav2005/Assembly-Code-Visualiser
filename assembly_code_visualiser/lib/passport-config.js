2f6bdfd215f79e073e2bbc5cb17cc94badf29c61
raghav2005 <raghavawasthi2005@gmail.com>
2022-08-10 21:39:24 +0400

    teacher login finished, but cannot test until teacher sign up completed

--- assembly_code_visualiser/lib/passport-config.js
+++ assembly_code_visualiser/lib/passport-config.js
@@ -4,7 +4,7 @@ var bcrypt = require('bcrypt');
 // for connecting to database
 var db_connection = require('./db');
 
-var get_user_by_email = (email) => {
+var get_student_by_email = (email) => {
 	return new Promise((resolve, reject) => {
 		try {
 			db_connection.query(
@@ -27,10 +27,33 @@ var get_user_by_email = (email) => {
 	});
 };
 
+var get_teacher_by_email = (email) => {
+	return new Promise((resolve, reject) => {
+		try {
+			db_connection.query(
+				'SELECT * FROM Teacher WHERE teacher_email = ?;', [email],
+				function (err, rows) {
+
+					if (err) {
+						console.log(err);
+						reject(err);
+					}
+
+					console.log(rows);
+
+					resolve(rows);
+				}
+			);
+		} catch (error) {
+			reject(error);
+		}
+	});
+};
+
 function initialize(passport) {
 	console.log('Initialized Passport..!');
 
-	var authenticate_user = async (req, email, password, done) => {
+	var authenticate_user_student = async (req, email, password, done) => {
 
 		// all user-side scripting for validation
 
@@ -56,7 +79,7 @@ function initialize(passport) {
 		// all server-side scripting for validation
 		try {
 
-			await get_user_by_email(email).then(async (rows) => {
+			await get_student_by_email(email).then(async (rows) => {
 
 				if (!rows || rows.length <= 0) {
 					return done(null, false, { message: ' No user with that email' });
@@ -86,16 +109,85 @@ function initialize(passport) {
 
 	};
 
-	passport.use(new LocalStrategy({
+	passport.use('local_student', new LocalStrategy({
 		usernameField: 'email',
 		passwordField: 'password',
 		passReqToCallback: true
 	},
-	authenticate_user));
+	authenticate_user_student));
+
+
+	var authenticate_user_teacher = async (req, email, password, done) => {
+
+		// all user-side scripting for validation
+
+		// ensure everything filled out
+		if (email.length === 0) {
+			return done(null, false, { message: ' Email is required ' });
+		}
+		if (password.length === 0) {
+			return done(null, false, { message: ' Password is required' });
+		}
+
+		// check email matches regex for teacher email
+		var regex_teacher = /^[A-Za-z]+\.[A-Za-z]+\@dubaicollege.org$/;
+		if (email.match(regex_teacher) === null) {
+			return done(null, false, { message: ' Invalid email format (DC email required)' });
+		}
+
+		// ensure password length is >= 8 characters
+		if (password.length < 8) {
+			return done(null, false, { message: ' Password length must be at least 8 characters' });
+		}
+
+		// all server-side scripting for validation
+		try {
+
+			await get_teacher_by_email(email).then(async (rows) => {
+
+				if (!rows || rows.length <= 0) {
+					return done(null, false, { message: ' No user with that email' });
+				}
+
+				try {
+
+					if (await bcrypt.compare(password, rows[0].student_password)) {
+						// everything correct from login
+						rows[0].role = 'teacher';
+						return done(null, rows[0])
+					} else {
+						return done(null, false, { message: ' Password incorrect' })
+					}
+
+				} catch (err) {
+					console.log(err);
+					return done(err)
+				}
+
+			});
+
+		} catch (error) {
+			console.log(error);
+			return done(null, false, { message: error });
+		}
+
+	};
+
+	passport.use('local_teacher', new LocalStrategy({
+		usernameField: 'email',
+		passwordField: 'password',
+		passReqToCallback: true
+	},
+		authenticate_user_teacher));
+
 
 	passport.serializeUser(function(user, done) {
 		process.nextTick(function() {
-			done(null, { id: user.student_id, email: user.student_email, role: user.role });
+			if (user.role === 'student') {
+				done(null, { id: user.student_id, email: user.student_email, role: user.role });
+			} else {
+				done(null, { id: user.teacher_id, email: user.teacher_email, role: user.role });
+			}
 		});
 	});
 
--- assembly_code_visualiser/routes/login_sign_up/login.js
+++ assembly_code_visualiser/routes/login_sign_up/login.js
@@ -15,12 +15,28 @@ router.get('/', auth.check_not_authenticated, function (req, res, next) {
 	res.render('login_sign_up/login', { title: 'Login', menu_id: 'login' });
 });
 
-// login form
-router.post('/', auth.check_not_authenticated, passport.authenticate('local', {
+// login form - student
+router.post('/student', auth.check_not_authenticated, passport.authenticate('local_student', {
 	failureRedirect: '/login',
 	failureFlash: true,
 	session: true
 }), function(req, res) {
+	// on successful authentication
+	// if remember me box not ticked
+	if (typeof req.body.remember_me === "undefined") {
+		res.redirect('/');
+	} else {
+		req.session.cookie.maxAge = 2592000000; // 30 days
+		res.redirect('/');
+	}
+});
+
+// login form - teacher
+router.post('/teacher', auth.check_not_authenticated, passport.authenticate('local_teacher', {
+	failureRedirect: '/login',
+	failureFlash: true,
+	session: true
+}), function (req, res) {
 	// on successful authentication
 	// if remember me box not ticked
 	if (typeof req.body.remember_me === "undefined") {

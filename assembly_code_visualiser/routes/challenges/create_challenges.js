3c83af59d728dba5f9f076b88dc2bdd3a9b2e526
raghav2005 <raghavawasthi2005@gmail.com>
2022-09-12 23:43:28 +0400

    completed basics of writing + deleting files

--- assembly_code_visualiser/routes/challenges/create_challenges.js
+++ assembly_code_visualiser/routes/challenges/create_challenges.js
@@ -6,6 +6,9 @@ var auth = require('../../lib/auth');
 // for connecting to database
 var db_connection = require('../../lib/db');
 
+// for writing to files
+var fs = require('fs');
+
 // coming to create_challenge page
 router.get('/', auth.check_authenticated, function (req, res, next) {
 
@@ -51,8 +54,48 @@ router.get('/new_challenge', auth.check_authenticated, function (req, res, next)
 // redirect to new_challenge page creation - testing stuffs
 router.post('/new_challenge/create', function (req, res, next) {
 
-	// code stuffs here
-	console.log('create');
+	var create_challenge_file_path = './public/text_files/teacher_create_challenge.txt';
+	var data = 'Hello !';
+
+	// check if file exists
+	
+	// ASYNCHRONOUS
+	// fs.access(create_challenge_file_path, fs.F_OK, (err) => {
+	// 	if (err) {
+	// 		console.error(err);
+	// 		return;
+	// 	};
+	// 	// file exists
+	// 	console.log('Teacher Create Challenge file already exists');
+	// });
+
+	// SYNCHRONOUS
+	try {
+		if (fs.existsSync(create_challenge_file_path)) {
+			// file exists
+			console.log('Teacher Create Challenge file already exists');
+			
+			try {
+				fs.unlinkSync(create_challenge_file_path);
+				// file removed
+				console.log('Teacher Create Challenge file deleted');
+			} catch (error) {
+				console.error(error);
+			};
+		}
+	} catch (err) {
+		console.error(err);
+	};
+
+
+	fs.writeFile(create_challenge_file_path, data, 
+	// callback function that is called after writing file is done
+	function (err) {
+		if (err) {
+			console.log(err);
+		};
+		console.log('Data written to Teacher Create Challenge file successfully')
+	});
 
 	// redirect to page to create a new challenge
 	res.redirect('/create_challenges');

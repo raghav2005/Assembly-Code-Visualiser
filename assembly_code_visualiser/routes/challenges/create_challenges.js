7e5fe49ea961fc975a5f78e46f6d577d5747d6a5
raghav2005 <raghavawasthi2005@gmail.com>
2022-12-28 01:04:53 +0400

    retrieved classes and students from database and displayed them as a table. TODO: convert to checkboxes and dropdown menus to be clicked to assign (see last comment)

--- assembly_code_visualiser/routes/challenges/create_challenges.js
+++ assembly_code_visualiser/routes/challenges/create_challenges.js
@@ -28,116 +28,200 @@ router.get('/', auth.check_authenticated, function (req, res, next) {
 					res.locals.message = req.flash();
 				}
 
-				// console.log('rows start');
-				// console.log(rows);
-				// console.log('rows end');
+				// console.log('TRY QUERY FOR GETTING CLASSES + STUDENTS OF TEACHER');
+				// console.log(req.user.email);
+				// console.log(req.user.id);
 
-				var challenges_to_display = [];
-				var challenges_only = [];
+				// store 2d list of [class (year + teacher id, e.g. 10RAW), class_id]
+				var classes = [];
 
-				rows.forEach(element => {
-					challenges_to_display.push([element['challenge_blob'].toString(), element['challenge_file_id']]);
-					challenges_only.push(element['challenge_blob'].toString());
-				});
-
-				// console.log('challenges_to_display start');
-				// console.log(challenges_to_display);
-				// console.log('challenges_to_display end');
-
-				var challenge_title_match_id = [];
-				
-				challenges_to_display.forEach(element => {
-
-					// separate challenge titles and descriptions
-					// challenge_titles.push(element.split('\n')[1]);
-					// challenge_descriptions.push(element.split('\n')[3]);
-
-					// console.log('in forEach loop');
-
-					// populate list of [challenge_title, challenge_file_id]
-					challenge_title_match_id.push([element[0].split('\n')[1], element[1]]);
-				});
-
-				// console.log(challenge_title_match_id);
-
-				// console.log(challenge_titles);
-				// console.log(challenge_descriptions);
-
-				challenges_to_display.forEach((element_new, index) => {
-
-					// console.log('inside here start');
-					// console.log(element_new, index);
-
-					var route_delete = '/delete/' + index.toString();
-
-					router.post(route_delete, function (req, res, next) {
-
-						// console.log('hi start');
-						// console.log(element_new);
-						// console.log('hi end');
-
-						// ! NOTE: THIS WORKS, EXCEPT WHEN THERE IS 1 REMAINING
-
-						db_connection.query(
-							'DELETE FROM Challenge_File WHERE challenge_file_id = ?;',
-							[element_new[1]],
-							function (err, rows) {
-								if (err) {
-									console.log(err);
-									req.flash('error', ' An error occured');
-									res.locals.message = req.flash();
-									return res.redirect('/create_challenges');
-								}
-								console.log(rows);
-							}
-						);
-
-						return res.redirect('/create_challenges');
-					});
-
-					var route_edit = '/edit/' + index.toString();
-
-					router.post(route_edit, function (req, res, next) {
-
-						// console.log('hi 2 start');
-						// console.log(element_new);
-						// console.log('hi 2 end');
-
-						res.locals.message = req.flash();
-
-						res.render('teacher_challenges/edit_challenge', {
-							title: 'Edit Challenge',
-							role: req.user.role,
-							email: req.user.email,
-							session_id: req.sessionID,
-							session_expiry_time: new Date(req.session.cookie.expires) - new Date(),
-							challenge_title: element_new[0].split('\n')[1].toString(),
-							challenge_description: element_new[0].split('\n')[3].toString(),
-							challenge_file_id: element_new[1].toString(),
+				// get classes of currently signed in teacher
+				db_connection.query(
+					'SELECT CONCAT(year_group, class_code), class_id FROM Class, Teacher WHERE Class.teacher_id = Teacher.teacher_id AND Class.teacher_id = ?;',
+					[req.user.id],
+					function (err_2, rows_2) {
+						
+						if (err_2) {
+							console.log(err_2);
+							req.flash('error', ' An error occured');
+							res.locals.message = req.flash();
+							return res.redirect('/create_challenges');
+						}
+						
+						// console.log(rows_2);
+						
+						rows_2.forEach((element, index) => {
+							classes.push([element['CONCAT(year_group, class_code)'], element['class_id']]);
 						});
 
-					});
+						// console.log('hi');
+						// console.log(classes);
 
-					// console.log('inside here end');
+						// console.log('rows start');
+						// console.log(rows);
+						// console.log('rows end');
 
-				});
+						// 2/3d list of [[class_id, [student1, student2, etc.]]]
+						var class_students = [];
 
-				// console.log('challenges_only start');
-				// console.log(challenges_only);
-				// console.log('challenges_only end');
+						classes.forEach((list_element, list_index) => {
 
-				res.render('teacher_challenges/create_challenges', {
-					title: 'Create Challenges',
-					menu_id: 'create_challenges',
-					role: req.user.role,
-					email: req.user.email,
-					// challenge_titles: challenge_titles,
-					// challenge_descriptions: challenge_descriptions,
-					challenges_to_display: challenges_only,
-					challenge_title_match_id: challenge_title_match_id,
-					session_id: req.sessionID,
-					session_expiry_time: new Date(req.session.cookie.expires) - new Date(),
-				});
+							// console.log(list_element[1]);
+
+							db_connection.query(
+								'SELECT CONCAT(student_name, student_number) FROM Student, Students_In_Classes WHERE Student.student_id = Students_In_Classes.student_id AND class_id = ?;',
+								[list_element[1]],
+								function (err_3, rows_3) {
+									
+									if (err_3) {
+										console.log(err_3);
+										req.flash('error', ' An error occured');
+										res.locals.message = req.flash();
+										return res.redirect('/create_challenges');
+									}
+
+									// console.log(rows_3);
+
+									var students = [];
+
+									rows_3.forEach((element, index) => {
+										students.push(element['CONCAT(student_name, student_number)']);
+									});
+
+									class_students.push([list_element[1], students]);
+									// console.log(class_students);
+
+									if (list_index == 3) {
+
+										// console.log(classes);
+										// console.log(class_students);
+										
+										var list_of_class_students = [];
+
+										classes.forEach((element, index) => {
+											list_of_class_students.push([classes[index][0], classes[index][1], class_students[index][1]]);
+										});
+
+										console.log(list_of_class_students);
+
+										var challenges_to_display = [];
+										var challenges_only = [];
+
+										rows.forEach(element => {
+											challenges_to_display.push([element['challenge_blob'].toString(), element['challenge_file_id']]);
+											challenges_only.push(element['challenge_blob'].toString());
+										});
+
+										// console.log('challenges_to_display start');
+										// console.log(challenges_to_display);
+										// console.log('challenges_to_display end');
+
+										var challenge_title_match_id = [];
+
+										challenges_to_display.forEach(element => {
+
+											// separate challenge titles and descriptions
+											// challenge_titles.push(element.split('\n')[1]);
+											// challenge_descriptions.push(element.split('\n')[3]);
+
+											// console.log('in forEach loop');
+
+											// populate list of [challenge_title, challenge_file_id]
+											challenge_title_match_id.push([element[0].split('\n')[1], element[1]]);
+										});
+
+										// console.log(challenge_title_match_id);
+
+										// console.log(challenge_titles);
+										// console.log(challenge_descriptions);
+
+										challenges_to_display.forEach((element_new, index) => {
+
+											// console.log('inside here start');
+											// console.log(element_new, index);
+
+											var route_delete = '/delete/' + index.toString();
+
+											router.post(route_delete, function (req, res, next) {
+
+												// console.log('hi start');
+												// console.log(element_new);
+												// console.log('hi end');
+
+												// ! NOTE: THIS WORKS, EXCEPT WHEN THERE IS 1 REMAINING
+
+												db_connection.query(
+													'DELETE FROM Challenge_File WHERE challenge_file_id = ?;',
+													[element_new[1]],
+													function (err, rows) {
+														if (err) {
+															console.log(err);
+															req.flash('error', ' An error occured');
+															res.locals.message = req.flash();
+															return res.redirect('/create_challenges');
+														}
+														console.log(rows);
+													}
+												);
+
+												return res.redirect('/create_challenges');
+											});
+
+											var route_edit = '/edit/' + index.toString();
+
+											router.post(route_edit, function (req, res, next) {
+
+												// console.log('hi 2 start');
+												// console.log(element_new);
+												// console.log('hi 2 end');
+
+												res.locals.message = req.flash();
+
+												res.render('teacher_challenges/edit_challenge', {
+													title: 'Edit Challenge',
+													role: req.user.role,
+													email: req.user.email,
+													session_id: req.sessionID,
+													session_expiry_time: new Date(req.session.cookie.expires) - new Date(),
+													challenge_title: element_new[0].split('\n')[1].toString(),
+													challenge_description: element_new[0].split('\n')[3].toString(),
+													challenge_file_id: element_new[1].toString(),
+												});
+
+											});
+
+											// console.log('inside here end');
+
+										});
+
+										// console.log('challenges_only start');
+										// console.log(challenges_only);
+										// console.log('challenges_only end');
+
+										res.render('teacher_challenges/create_challenges', {
+											title: 'Create Challenges',
+											menu_id: 'create_challenges',
+											role: req.user.role,
+											email: req.user.email,
+											// challenge_titles: challenge_titles,
+											// challenge_descriptions: challenge_descriptions,
+											challenges_to_display: challenges_only,
+											challenge_title_match_id: challenge_title_match_id,
+											class_students: list_of_class_students,
+											session_id: req.sessionID,
+											session_expiry_time: new Date(req.session.cookie.expires) - new Date(),
+										});
+									
+									};
+
+								}
+							);
+
+						});
+
+					}
+
+				);
 
 			}
 		);

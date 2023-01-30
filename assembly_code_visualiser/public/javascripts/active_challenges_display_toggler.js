2b9d25e0c89b773ce3bd1dd6877b242a23169ef0
raghav2005 <raghavawasthi2005@gmail.com>
2023-01-31 02:17:13 +0400

    all teacher side css DONE

--- assembly_code_visualiser/public/javascripts/active_challenges_display_toggler.js
+++ assembly_code_visualiser/public/javascripts/active_challenges_display_toggler.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
 	$('#active_challenges_card_body').hide();
 	$('#active_challenges').hide();
+	$('#challenges_havent_been_assigned').hide();
+	$('#students_have_submitted').hide();
+	$('#students_have_submitted_table').hide();
+	$('#students_yet_to').hide();
+	$('#students_yet_to_table').hide();
+	$('#students_missed').hide();
+	$('#students_missed_table').hide();
 });
 
 function toggle_display_challenges() {
@@ -22,3 +29,61 @@ function toggle_assign_challenges() {
 		$('#assign_challenges_toggle').text("Show Assign Challenges");
 	};
 };
+
+function toggle_challenges_have_been_assigned() {
+	$('#challenges_have_been_assigned').toggle();
+	if ($('#display_challenges_have_been_toggle').text() == "Show Challenges" || $('#display_challenges_have_been_toggle').text() === "Show Challenges") {
+		$('#display_challenges_have_been_toggle').text("Hide Challenges");
+	} else {
+		$('#display_challenges_have_been_toggle').text("Show Challenges");
+	};
+};
+
+function toggle_challenges_havent_been_assigned() {
+	$('#challenges_havent_been_assigned').toggle();
+	if ($('#display_challenges_havent_been_toggle').text() == "Show Challenges" || $('#display_challenges_havent_been_toggle').text() === "Show Challenges") {
+		$('#display_challenges_havent_been_toggle').text("Hide Challenges");
+	} else {
+		$('#display_challenges_havent_been_toggle').text("Show Challenges");
+	};
+};
+
+function toggle_challenge_info() {
+	$('#challenge_info').toggle();
+	if ($('#display_challenge_info').text() == "Show Information" || $('#display_challenge_info').text() === "Show Information") {
+		$('#display_challenge_info').text("Hide Information");
+	} else {
+		$('#display_challenge_info').text("Show Information");
+	};
+}
+
+function toggle_students_have_submitted() {
+	$('#students_have_submitted').toggle();
+	$('#students_have_submitted_table').toggle();
+	if ($('#display_students_have_submitted').text() == "Show Students" || $('#display_students_have_submitted').text() === "Show Students") {
+		$('#display_students_have_submitted').text("Hide Students");
+	} else {
+		$('#display_students_have_submitted').text("Show Students");
+	};
+}
+
+function toggle_students_yet_to() {
+	$('#students_yet_to').toggle();
+	$('#students_yet_to_table').toggle();
+	if ($('#display_students_yet_to').text() == "Show Students" || $('#display_students_yet_to').text() === "Show Students") {
+		$('#display_students_yet_to').text("Hide Students");
+	} else {
+		$('#display_students_yet_to').text("Show Students");
+	};
+}
+
+function toggle_students_missed() {
+	$('#students_missed').toggle();
+	$('#students_missed_table').toggle();
+	if ($('#display_students_missed').text() == "Show Students" || $('#display_students_missed').text() === "Show Students") {
+		$('#display_students_missed').text("Hide Students");
+	} else {
+		$('#display_students_missed').text("Show Students");
+	};
+}
+

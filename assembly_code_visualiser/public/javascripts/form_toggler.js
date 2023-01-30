85af86b18cbcb90a3d7504bdb6bb79c495b24a26
raghav2005 <raghavawasthi2005@gmail.com>
2023-01-30 23:09:37 +0400

    styling login pages complete

--- assembly_code_visualiser/public/javascripts/form_toggler.js
+++ assembly_code_visualiser/public/javascripts/form_toggler.js
@@ -1,9 +1,12 @@
 // immediately hide teacher form when page loads
 $(document).ready(function () {
 	$('#teacher_form').hide();
+	$('#teacher_card').hide();
 });
 
 function change_form_login() {
 	$('#teacher_form').toggle();
 	$('#student_form').toggle();
+	$('#teacher_card').toggle();
+	$('#student_card').toggle();
 }

540a58cd0d2cfde12d496ce061f326b5dd2c60e1
raghav2005 <raghavawasthi2005@gmail.com>
2023-01-31 01:24:29 +0400

    styling teacher create challenges page (creating new challenge + assigning) complete

---
+++ assembly_code_visualiser/public/javascripts/active_challenges_display_toggler.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+	$('#active_challenges_card_body').hide();
+	$('#active_challenges').hide();
+	$('#assign_challenges_card_body').hide();
+	$('#assign_challenge_form').hide();
+});
+
+function toggle_display_challenges() {
+	$('#active_challenges_card_body').toggle();
+	$('#active_challenges').toggle();
+	if ($('#display_challenges_toggle').text() == "Show Challenges" || $('#display_challenges_toggle').text() === "Show Challenges") {
+		$('#display_challenges_toggle').text("Hide Challenges");
+	} else {
+		$('#display_challenges_toggle').text("Show Challenges");
+	};
+};
+
+function toggle_assign_challenges() {
+	$('#assign_challenges_card_body').toggle();
+	$('#assign_challenge_form').toggle();
+	if ($('#assign_challenges_toggle').text() == "Show Assign Challenges" || $('#assign_challenges_toggle').text() === "Show Assign Challenges") {
+		$('#assign_challenges_toggle').text("Hide Assign Challenges");
+	} else {
+		$('#assign_challenges_toggle').text("Show Assign Challenges");
+	};
+};
